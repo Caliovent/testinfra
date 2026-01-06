c0407d819571cce5d91188edfb6cfb9a034059bb
Vincent Schmitt <vschmitt@fortinet.com>
2026-01-06 10:35:18 +0100

    fix deprecated in favor of `floating_ip_enabled`

--- network.tf
+++ network.tf
@@ -67,9 +67,7 @@ resource "azurerm_lb_rule" "lbnatrule" {
   frontend_ip_configuration_name = "LoadBalancerFrontEnd"
   probe_id                       = azurerm_lb_probe.elb_probe.id
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.elb_backend.id]
-
-  # CRITICAL: Enable Floating IP so packets arrive with Destination IP = Public IP
-  enable_floating_ip = true
+  floating_ip_enabled            = true
 }
 
 // --- NETWORK SECURITY GROUPS ---
